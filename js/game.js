3441a138e072e070ca6394fe0af593e3c983d4e3
Nathan Alleyne <nathan.alleyne7@gmail.com>
2019-03-06 22:53:58 +0000

    Started, cleaned up, almost finished

--- js/game.js
+++ js/game.js
@@ -1,46 +1,113 @@
 // NOTE - You must have a level with the name "start".  This is used as the first level in the game.
 
 var game = {
-    music: "98_Lost_Mine.mp3",
+    music: "Stranger-Things-Theme.mp3",
     background_image: "intro-bg.jpg",
     levels: {
 
         start: {
-            message: "You come across a dark cave",
+            background_image:"videoblocks-adult-caucasian-man-talking-on-mobile-phone-late-at-night-while-lying-in-bed-in-bedroom-selective-focus-on-alarm-clock-on-bedside-table_b-g3pq2rw_thumbnail-small01.jpg",
+            
+            message: "You wake up in the middle of the night and you're having trouble going back to bed.",
             choices: [
                 {
-                    text: "Enter the cave",
-                    nextLevel: "cave",
+                    text: "Get out of bed.",
+                    nextLevel: "hallwayUp",
                 },
 
                 {
-                    text: "Keep on moving",
-                    nextLevel: "field",
+                    text: "Keep trying to go back to sleep.",
+                    nextLevel: "backToSleep",
                 },
             ]
         },
 
-        cave: {
-            background_image: "fire.gif",
-            music: "Final-Fantasy-7-Boss-Battle.mp3",
-            message: "You come across a fire monster or something!",
+        hallwayUp: {
+            background_image: "scary-house-hallway-camera-traveling-high-definition-38343707.jpg",
+            message: "Go to the bathroom and wash your face or go downstairs to grab a snack?",
             choices: [
                 {
-                    text: "Start over",
+                    text: "Go to bathroom",
+                    nextLevel: "bathroom",
+                },
+                {
+                    text: "Go downstairs",
+                    nextLevel: "downstairs",
+                },
+            ]
+        },
+        
+        backToSleep: {
+            message: "Goodnight...",
+            choices: [
+                {
+                    text: "Replay",
                     nextLevel: "start",
                 },
             ]
         },
-
-        field: {
-            message: "Some adventurer you are...",
+        
+        bathroom: {
+            message: "You've washed your face. Now what?",
             choices: [
                 {
-                    text: "Start over",
+                    text: "Back to bed.",
+                    nextLevel: "backToSleep",
+                },
+                {
+                    text: "Grab a snack downstairs.", 
+                    nextLevel: "downstairs",
+                },
+            ]
+        },
+        
+        downstairs: {
+            message: "You find that the lights aren't working downstairs.",
+            choices: [
+                {
+                    text: "Go back to bed",
+                    nextLevel: "backToSleep",
+                },
+                {
+                    text:"Find a flashlight and investigate", 
+                    nextLevel: "intruder",
+                },
+            ]
+        },
+        intruder: {
+            message: "You spot an intruder! What do you do?",
+            choices: [
+                {
+                    text: "Stand shocked, paralyzed, still.",
+                    nextLevel: "death",
+                },
+                {
+                    text:"Attack him with the first thing you see, a TV Remote.",
+                    nextLevel: "victory",
+                },
+            ]
+        },
+        death: {
+            message: "He found you, and you didn't make it. RIP.",
+            choices: [
+                {
+                    text: "Replay",
                     nextLevel: "start",
                 },
             ]
         },
+        
+        victory: {
+            message: "You knock him unconscious, call the police, and all is well.",
+            choices: [
+                {
+                    text: "Replay",
+                    nextLevel: "start",
+                },
+            ]
+        },
+        
+        
 
     }
 };
